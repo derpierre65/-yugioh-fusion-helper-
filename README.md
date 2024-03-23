8b856a6935f30904eb9580f1f1622ca73c0e199e
derpierre65 <git@derpierre65.de>
2024-03-23 09:10:03 +0000

    chore: init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Yu-Gi-Oh! Fusion helper
+Inspired by the project [ignaciora/yugioh-fusion-helper](https://github.com/ignaciora/yugioh-fusion-helper/)  
+Given a set of card ids, it gets every fusion of the game "Yu-Gi-Oh! Forbidden Memories" (PlayStation 1).  
+Have fun with retro games! :)
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Yu-Gi-Oh! Forbidden Memories Fusion Helper
+        </q-toolbar-title>
+
+        <div>v0.1.0</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+</script>
